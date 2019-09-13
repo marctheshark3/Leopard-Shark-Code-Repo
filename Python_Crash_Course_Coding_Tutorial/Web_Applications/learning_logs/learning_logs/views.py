ce6d9583f13486d6800c9c0bb23d4c37bc3549f3
Marc Mailloux <marcmailloux@knights.ucf.edu>
2019-09-13 14:25:30 -0600

    web_dev

--- Python_Crash_Course_Coding_Tutorial/Web_Applications/learning_logs/learning_logs/views.py
+++ Python_Crash_Course_Coding_Tutorial/Web_Applications/learning_logs/learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """The hope page for Learning Log"""
+    return render(request, 'learning_logs/index.html')
