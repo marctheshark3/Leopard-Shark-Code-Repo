28b25da3c44071788cad7eb426b9ea38a869cf24
Marc Mailloux <marcmailloux@knights.ucf.edu>
2019-09-14 11:25:47 -0600

    Removal of learning logs

--- Python_Crash_Course_Coding_Tutorial/Web_Applications/learning_logs/learning_logs/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    """The hope page for Learning Log"""
-    return render(request, 'learning_logs/index.html')
